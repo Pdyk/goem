80a02bb483f4fc7fc4ded6ad86b111e66aaf32a5
Pdyk <101911219+Pdyk@users.noreply.github.com>
2022-03-23 22:26:46 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5280973535:AAGO1udz6eeTfQcTktDCGtRyYvVqLx4J638' #token de tu bot
+API_ID = 14681595 # lo creas en https://my.telegram.org/
+API_HASH = 'a86730aab5c59953c424abb4396d32d5'  # lo creas en https://my.telegram.org/
+OWNER = [1759969205]
+CHAT_ID = '-1001666279733'
+
+# DATOS DEL MOODLE
+USUARIO = 'jayala '
+MOODLE_PASSWORD = 'damarys/1'
+USUARIO_ID = 3588 #Tu id
+ZIP_MB = 399
+MOODLE_URL = 'https://moodle.uclv.edu.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = 'AQUI TU GMAIL' 
+PASS_MEGA = 'AQUI TU PASS'
